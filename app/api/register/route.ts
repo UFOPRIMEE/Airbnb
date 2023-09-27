7f174357416372ce5370edff8edd309dc6e5326d
Aditya Joshi <aditya03joshi@gmail.com>
2023-09-27 23:59:02 +0530

    created app/api/register/route.ts and pages/api/auth[...nextauth].ts

---
+++ app/api/register/route.ts
@@ -0,0 +1,24 @@
+import {NextResponse} from "next/server";
+import bcrypt from "bcrypt";
+import prisma from "@app/libs/prismadb";
+
+export async function POST(
+  request: Request,
+) {
+  const body = request await.json() ;
+  const {
+    email,
+    name, 
+    password,
+  } = body ; 
+  const hashedPassword = await bcrypt.hash(password,12);
+  
+  const user = await prism.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    }
+  });
+  return NextResponse.json(user);
+}
